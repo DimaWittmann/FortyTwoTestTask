665bcc86bd9408ee5b43f0525618b2454590d2e8
DimaWittmann <dimawittmann@gmail.com>
2015-07-03 12:04:52 +0300

    ticket:1 update persons table at admin page

--- apps/personal_page/admin.py
+++ apps/personal_page/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
 from apps.personal_page.models import Person
 
-admin.site.register(Person)
+
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ('name', 'last_name', 'date_of_birth', 'email',
+                    'jabber', 'skype')
+    list_display_links = ('last_name',)
+
+
+admin.site.register(Person, PersonAdmin)
